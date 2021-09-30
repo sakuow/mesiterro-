672408eba3efc957047842dcb1721276146e618a
sakuow <yusaku1335@gmail.com>
2021-09-30 07:38:26 +0000

    3 commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '610082611392736028829c14004bb1f44b056e036811c4adf969abbac2837e82f17abb4da8103bddc61dc016e3c5b94cbe1ca1dc9404da066b2ca829fac04da9'
